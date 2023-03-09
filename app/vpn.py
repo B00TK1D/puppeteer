a5817303fd4f9087c3296f5686ea1e7c3b918df5
B00TK1D <stearns.josiah@gmail.com>
2023-03-09 16:37:24 -0700

    Added VPN status

--- app/vpn.py
+++ app/vpn.py
@@ -1,18 +1,38 @@
 import os
 import flask
+import psutil
 import subprocess
-import threading
 
-import db
 import utils
 import settings
 
 connect_p = None
 
+list(filter(lambda x: 'Chrome' in x.name, [p for p in psutil.process_iter()]))
+
+# Helper functions
+def get_status():
+    # List interfaces
+    addrs = psutil.net_if_addrs()
+    # List processes
+    procs = [p for p in psutil.process_iter()]
+    # Filter processes for openvpn
+    openvpn_procs = list(filter(lambda x: 'openvpn' in x.name(), procs))
+    if "tun0" in addrs:
+        if len(openvpn_procs) > 0:
+            return "Connected"
+        else:
+            return "Stale connection"
+    else:
+        if len(openvpn_procs) > 0:
+            return "Connecting..."
+    return "Disconnected"
+
 # Frontend views
 def view_vpn():
     code = utils.read_file_contents(os.path.join(settings.VPN_CONNECT_FILE))
-    return flask.render_template("vpn.html", code = code)
+    status = get_status()
+    return flask.render_template("vpn.html", code = code, status = status)
 
 
 # Backend functions
@@ -25,6 +45,7 @@ def connect_vpn():
 
     return flask.render_template("vpn.html", code = code, messages = ["VPN connected"])
 
+
 def disconnect_vpn():
     global connect_p
     connect_p.kill()
@@ -32,6 +53,7 @@ def disconnect_vpn():
     code = utils.read_file_contents(os.path.join(settings.VPN_CONNECT_FILE))
 
     return flask.render_template("vpn.html", code = code, messages = ["VPN disconnected"])
+
 
 def update_vpn():
     code = flask.request.form.get("code")
