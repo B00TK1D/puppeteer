d0651bbc81f04c394f6bbd41f30e1793b70a1905
B00TK1D <stearns.josiah@gmail.com>
2023-03-09 21:19:20 -0700

    Added VPN seperate modify page

--- app/vpn.py
+++ app/vpn.py
@@ -52,7 +52,7 @@ def connect_vpn():
 
     code = utils.read_file_contents(os.path.join(settings.VPN_CONNECT_FILE))
 
-    return flask.render_template("vpn.html", code = code, messages = ["VPN connected"])
+    return flask.render_template("vpn/index.html", code = code, messages = ["VPN connected"])
 
 
 def disconnect_vpn():
@@ -63,7 +63,7 @@ def disconnect_vpn():
 
     code = utils.read_file_contents(os.path.join(settings.VPN_CONNECT_FILE))
 
-    return flask.render_template("vpn.html", code = code, messages = ["VPN disconnected"])
+    return flask.render_template("vpn/index.html", code = code, messages = ["VPN disconnected"])
 
 
 def update_vpn():
@@ -71,11 +71,7 @@ def update_vpn():
 
     if "file" in flask.request.files:
         file = flask.request.files["file"]
-
         file.save(settings.VPN_CONFIG_FILE)
-
-        print("Saved vpn file")
-
     utils.write_file_contents(os.path.join(settings.VPN_CONNECT_FILE), code)
 
     return flask.redirect("/vpn/modify?message=VPN+updated")
