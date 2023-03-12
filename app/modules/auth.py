de994ad866a9bd6208c2eb64c9d7672460cacef2
B00TK1D <stearns.josiah@gmail.com>
2023-03-11 19:53:18 -0700

    Logout after password change

--- app/modules/auth.py
+++ app/modules/auth.py
@@ -125,6 +125,6 @@ def change_password():
     hashed = bcrypt.hashpw(password.encode('utf-8'), bcrypt.gensalt())
     username = current_user()["username"]
     users[username]["hash"] = hashed
-    response = flask.make_response(flask.redirect('/login'))
+    response = flask.make_response(flask.redirect('/login?messsages=Password+changed'))
     response.set_cookie('auth', '', expires=0)
     return response
